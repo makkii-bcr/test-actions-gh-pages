e9c6f4e88e5f0735df2ff71e91af42f7213931ea
makkii_bcr <tonyu.makkii@gmail.com>
2021-04-12 23:46:33 +0900

    ccc

--- conv.js
+++ conv.js
@@ -5,6 +5,7 @@ import path from 'path';
 // markdown -> html 変換
 const mdData = fs.readFileSync('conv.js');
 const htmlData = marked(mdData.toString());
+fs.mkdirSync('docs', { recursive: true });
 fs.writeFileSync(path.join('docs', 'aaa.html'), htmlData);
 
 
